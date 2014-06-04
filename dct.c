6e327f465c7cc82abee78ad7894c101a0378582f
Massimiliano Scotti <massimilianoscotti@gmail.com>
2014-06-04 23:15:36 +0200

    per convenzione le matrici sono mxn :)

--- dct.c
+++ dct.c
@@ -6,8 +6,8 @@
 
 int main(void)
 {
-    int n = 3, m = 4;
-    int size = n * m;
+    int m = 3, n = 4;
+    int size = m * n;
     double * out;
 
     fftw_plan plan;
@@ -16,36 +16,36 @@ int main(void)
 
     double in[] = {11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34}; 
     
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            printf("%8.3f ", in[i*m + j]);
-	   }
-	   putchar('\n');
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            printf("%8.3f ", in[i*n + j]);
+        }
+        putchar('\n');
     }
     printf("\n\n");
     
-    plan = fftw_plan_r2r_2d(n, m, in, out, FFTW_REDFT10,
+    plan = fftw_plan_r2r_2d(m, n, in, out, FFTW_REDFT10,
 			    FFTW_REDFT10, FFTW_ESTIMATE);
 
     fftw_execute(plan);
 
     double norm1 = 4 * sqrt(m/2.0) * sqrt(n/2.0);
     double norm2 = sqrt(2);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            out[i*m + j] = out[i*m + j] / norm1;
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            out[i*n + j] = out[i*n + j] / norm1;
             if(i == 0 || j == 0)
-                out[i*m + j] = out[i*m + j] / norm2;
+                out[i*n + j] = out[i*n + j] / norm2;
         }
     }
     out[0] = out[0] / norm2;
     
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            printf("%8.3f ", out[i*m + j]);
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            printf("%8.3f ", out[i*n + j]);
         }
-	   putchar('\n');
+        putchar('\n');
     }
     printf("\n");
     
